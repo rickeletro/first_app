ffc2abd8c3c609554d07cc9ce85462fe88f34dd8
rickeletro <rickeletro@gmail.com>
2015-03-16 20:35:18 -0300

    rota black

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
-def index
-  
-end
+	def index
+	  
+	end
+	def black
+		render :layout => 'application_black'		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root :to => 'welcome#index', :as => :index
+  match 'black' => 'welcome#black', :via =>
+  :get, :as => :black
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
